a7499388c37452c4ac1eeb73bbf1e7aaa39c90ef
BryannaKFox <146906807+BryannaKFox@users.noreply.github.com>
2024-01-08 21:23:35 -0800

    Lockscreen

---
+++ LockScreen.ps1
@@ -0,0 +1,8 @@
+# Script:                       ops401d10-Lab01
+# Author:                       Bryanna Fox
+# Date of latest revision:      1/8/2024
+# Purpose:                      Automation to lock screen
+
+# Trigger a lock to the computer screen
+rundll32.exe user32.dll, LockWorkStation
+
